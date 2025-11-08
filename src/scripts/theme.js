31f607660d7a6408b99dacb3bac78d3e8b0dd350
Eris <hi@eris.cafe>
2025-11-08 00:11:48 -0700

    theme stuff

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -8,14 +8,24 @@ export function setTheme(theme) {
 
 export function applyTheme() {
     const theme = settings.get('theme');
+    const metaThemeColor = document.querySelector("meta[name=theme-color]");
+    if (metaThemeColor) {
+        metaThemeColor.setAttribute("content", "#0f0f14");
+    }
 
     document.body.classList.remove('light');
     if (theme === 'system') {
         if (window.matchMedia('(prefers-color-scheme: light)').matches) {
             document.body.classList.add('light');
+            if (metaThemeColor) {
+                metaThemeColor.setAttribute("content", "#E9E9EC");
+            }
         }
     } else if (theme === 'light') {
         document.body.classList.add('light');
+        if (metaThemeColor) {
+            metaThemeColor.setAttribute("content", "#E9E9EC");
+        }
     }
 
     if (content.dataset.page === 'settings') {
