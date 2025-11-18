77d76da79723652904c20ecf344f0767f54d9e2c
Eris <hi@eris.cafe>
2025-11-18 08:11:15 -0700

    fix more notification bugs

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -884,7 +884,7 @@ function createNotification(data) {
                 <div class="notification-content">
                     <span class="notification-title">${data.data.actor.name} ${notificationTypes[data.type]}</span>
                     <div class="notification-post post-content">
-                        ${data.data.comment.content || '<p style="font-style: italic">but the comment was deleted :(</p>'}
+                        ${data?.data?.comment?.content || '<p style="font-style: italic">but the comment was deleted :(</p>'}
                     </div>
                 </div>
             </div>
